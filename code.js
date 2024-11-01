82585f30f42972f7697dac08d5ebe88f47c126de
LukeHoese <157862417+LukeHoese@users.noreply.github.com>
2024-11-01 12:34:34 -0600

    gotta start somewhere

--- code.js
+++ code.js
@@ -1,3 +1,20 @@
 function binarySearch(list, element) {
+    var listStart = 0;
+    var listEnd = (list.length - 1);
+
+    while (listStart <= listEnd) {
+        var listMid = ((listStart + listEnd) / 2);
+        // element found, return index
+        if (list[listMid] === element) {
+            return listMid;
+        }
+        // element smaller than mid, adjust search to smaller half of list
+        if (element < list[listMid]) {
+            listEnd = mid - 1;
+        }
+        // element larger than mid, adjust search to larger half of list
+        else if (element > list[listMid] {
+            listStart = mid + 1;
+        }
     return -1;
 }
